0ad2b08e61653477f794460c639039240f3a4529
Fabian <L18TE0169@teziutlan.tecnm.mx>
2021-11-12 13:42:45 -0600

    hice cambios

---
+++ Principal.py
@@ -0,0 +1,8 @@
+"""
+El gerente de la empresa es un empleado que mide 1.80 m, se llama <nombre> y tiene puesto un saco con tres botones.
+En este momento el gerente está entrevistando a una persona para su contratación.”
+"""
+
+class Persona:
+    def __init__(self, altura):
+        self.__altura = altura
---
+++ main_Principal.py
@@ -0,0 +1,7 @@
+
+import Principal as main_Principal
+"""
+El gerente de la empresa es un empleado que mide 1.80 m, se llama <nombre> y tiene puesto un saco con tres botones.
+En este momento el gerente está entrevistando a una persona para su contratación.”
+"""
+print("Problema: ")

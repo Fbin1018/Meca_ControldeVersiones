ced1ff83bcb8af67e3737c6e8e817fb91570602f
Fabian <L18TE0169@teziutlan.tecnm.mx>
2021-11-12 23:59:51 -0600

    hice cambios, 5

--- Principal.py
+++ Principal.py
@@ -10,35 +10,51 @@ class Persona:
         self.__edad = edad
 
     def getNombre(self):
-        return self.__nombre
-    def hetEdad(self):
-        return self.__edad
-    def getaltura(self):
-        return self.__altura
+        print(self.__nombre)
+    def getEdad(self):
+        print(self.__edad)
+    def getAltura(self):
+        print(self.__altura)
 
     def setEdad(self, nuevaEdad):
-        self.__edad=nuevaEdad
-    def setNaltura(self, nuevaLtura):
-        self.__edad=nuevaLtura
+        self.__edad = nuevaEdad
+    def setAltura(self, nuevaAltura):
+        self.__edad = nuevaAltura
 
 
 class Empleado(Persona):
     def __init__(self, nombre, altura, edad, nivel):
-        super().__init__(nombre, altura, edad)
+        Persona.__init__(self, nombre, altura, edad)
         self.__nivel = nivel
 
     def Nivel(self):
-        return self.__nivel
+        print(self.__nivel)
 
     def setCargo(self, nuevoNivel):
         self.__nivel=nuevoNivel
 
 class Gerente(Empleado):
     def __init__(self, nombre, altura, edad, nivel, depto):
-        super().__init__(self, nombre, altura, edad, nivel)
+        Empleado.__init__(self, nombre, altura, edad, nivel)
+        self.__depto = depto
+        self.__nombre = nombre
+
+    def getNombre(self):
+        Persona.getNombre(self)
+    def getEdad(self):
+        Persona.getEdad(self)
+    def getAltura(self):
+        Persona.getAltura(self)
+
+    def Introduccion(self):
+        print("El gerente de la empresa es un empleado que mide 1.80 m, se llama",self.__nombre, "y tiene puesto un saco con tres botones."
+                                                                                                 "\nEn este momento el gerente está entrevistando a una persona para su contratación.")
 
     def Vestimenta(self):
-        print("El gerente tiene puesto un saco con tres botones.")
+        print("El gerente", self.__nombre, " tiene puesto un saco con tres botones.")
 
     def Departamento(self):
-        print("El gerente es del departamento de: ")
+        print("El gerente es del departamento de: ", self.__depto)
+
+    def Problema(self):
+        print("Problema: ",self.__nombre)
--- main_Principal.py
+++ main_Principal.py
@@ -2,6 +2,11 @@
 import Principal as main_Principal
 """
 El gerente de la empresa es un empleado que mide 1.80 m, se llama <nombre> y tiene puesto un saco con tres botones.
-En este momento el gerente está entrevistando a una persona para su contratación.”
+En este momento el gerente está entrevistando a una persona para su contratación.
 """
-print("Problema: ")
+
+gerente = main_Principal.Gerente("Gerardo",1.80, 39, 5, "Finanzas")
+gerente.Problema()
+gerente.Introduccion()
+persona = main_Principal.Persona("Jose", 1.78, 30)
+persona.getNombre()
